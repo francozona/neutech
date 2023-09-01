7976b902d2723fcac29b99219b561fc8caecc229
neutechno <francozona1@gmail.com>
2023-09-01 08:52:49 +0100

    update on the url fomart

---
+++ _site/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# You can add any setup or configuration steps you need here
+# For example, installing additional gems or dependencies
+
+# Execute the command passed to the script (e.g., "bundle exec jekyll serve")
+exec "$@"
+
